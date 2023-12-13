ddda288923b7ac97d50461470db34e7dea66e061
tanea09 lewis <tlewis25@livingschoolnola.org>
2023-12-13 20:42:32 +0000

    test

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,12 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+     for (let i = 100; i < canvas.width; i += 100) {
+       createPlatform(i, canvas.height, -1, -canvas.height);
+     }
+     for (let i = 100; i < canvas.height; i += 100) {
+       createPlatform(canvas.width, i, -canvas.width, -1);
+     }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -44,15 +44,23 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
+createPlatform(500,650,100,10)
+createPlatform(250,700,100,10)
+createPlatform(700,550,100,10)
+createPlatform(900,450,100,10)
+createPlatform(700,390,100,10)
+createPlatform(500,330,100,10)
+createPlatform(700,220,300,10)
+createPlatform(1100,220,10,900)
+createPlatform(700,700,100,10)
 
-
-    
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+createCollectable("database",250,200,1,0.5)
+createCollectable("diamond", 750,230,1,0.5)
 
 
 
@@ -61,6 +69,11 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+createCannon("left",400,1000,50,30)
+createCannon("top",500,1000,20,50)
+createCannon("left",100,2000,50,30)
+createCannon("right",800,700,1500)
+
 
 
 
